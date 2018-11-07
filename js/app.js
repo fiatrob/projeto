8621ad68332e35255cb4010883cb7b391f627ca2
fiatrob <track.almeida@gmail.com>
2018-11-07 19:33:20 -0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBCnPMFGAXt3nPwRc0RQBEHRLxBjeN-u0E",
+    authDomain: "projetofaculdade-44585.firebaseapp.com",
+    databaseURL: "https://projetofaculdade-44585.firebaseio.com",
+    projectId: "projetofaculdade-44585",
+    storageBucket: "projetofaculdade-44585.appspot.com",
+    messagingSenderId: "818782664809"
+};
+firebase.initializeApp(config);
